a46817583dcfefe599d02c467f93f4a708ff635c
Tanay <tanay.joshi9@gmail.com>
2022-01-25 12:02:16 +0530

    added badge component

---
+++ badges/script.js
@@ -0,0 +1,12 @@
+const btnSecondary = document.querySelector('.btn-secondary');
+
+btnSecondary.addEventListener('click', () => {
+    const badgeElement = btnSecondary.children[0];
+
+    if(badgeElement.hasAttribute('class')){
+        badgeElement.removeAttribute('class')
+    }else{
+        badgeElement.classList.add('badge');
+    }
+
+})
